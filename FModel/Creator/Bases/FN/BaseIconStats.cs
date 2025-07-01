052de8a36700a3b2bbd6523a2e7315f9a08c79a2
Chompster86 <chompster86@gmail.com>
2025-07-01 16:28:03 -0400

    Fixed if firing rate = 1
    Made it so if firing rate = 1, it hides the burst firing rate equation and just shows the firing rate.

--- FModel/Creator/Bases/FN/BaseIconStats.cs
+++ FModel/Creator/Bases/FN/BaseIconStats.cs
@@ -130,6 +130,11 @@ public class BaseIconStats : BaseIcon
                 _statistics.Add(new IconStat(Utils.GetLocalizedResource("", "27B80BA44805ABD5A2D2BAB2902B250C", "Fire Rate"), burstEquation, 11));
             }
 
+            else if (firingRate != 0f)
+            {
+                _statistics.Add(new IconStat(Utils.GetLocalizedResource("", "27B80BA44805ABD5A2D2BAB2902B250C", "Fire Rate"), firingRate, 11));
+            }
+
             if (armTime != 0f)
             {
                 _statistics.Add(new IconStat(Utils.GetLocalizedResource("", "3BFEB8BD41A677CC5F45B9A90D6EAD6F", "Arming Delay"), armTime, 5));
